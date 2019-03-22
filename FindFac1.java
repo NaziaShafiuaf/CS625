91d188d0ad25ed4b7967232faf7e8e3aef68b75b
NaziaShafiuaf <47970809+NaziaShafiuaf@users.noreply.github.com>
2019-03-22 17:30:36 +0500

    Add files via upload

---
+++ FindFac1.java
@@ -0,0 +1,10 @@
+class FindFac1 {
+public static void main(String args[]) {
+for(int i=2; i <= 100; i++) {
+System.out.println("Factorial of " + i + ": ");
+for(int j = 2; j < i; j++)
+if((i%j) == 0) System.out.println(j + " ");
+System.out.println();
+}
+}
+}
